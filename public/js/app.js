79ec647ad9435ce4e584907c1337751d959c6070
Alexander Andreev <cahekec39@gmail.com>
2019-11-28 15:58:36 +0200

    init repository

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const locationParagraph = document.getElementById("location");
+const forecastParagraph = document.getElementById("forecast");
+
+weatherForm.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const location = search.value;
+
+  locationParagraph.textContent = "Loading...";
+
+  try {
+    fetch(`http://localhost:4000/weather?address=${location}`).then(
+      response => {
+        response.json().then(data => {
+          if (data.error) {
+            locationParagraph.textContent = data.error;
+            forecastParagraph.textContent = "";
+          } else {
+            locationParagraph.textContent = data.location;
+            forecastParagraph.textContent = data.forecast;
+          }
+        });
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+});
