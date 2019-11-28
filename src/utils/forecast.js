d5e1543e7f942da45ebae0dbff891b446ff6c260
Alexander Andreev <cahekec39@gmail.com>
2019-11-28 17:09:23 +0200

    add min and max temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,12 +9,18 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
       callback("Unable to find location", undefined);
     } else {
+      console.log(body.daily.data[0]);
+
       callback(
         undefined,
         body.daily.data[0].summary +
           " It is currently " +
           body.currently.temperature +
-          " degress out. There is a " +
+          " degress out with " +
+          body.daily.data[0].temperatureMin +
+          " degress min tempetarute and " +
+          body.daily.data[0].temperatureMax +
+          " max tempetarute. There is a " +
           body.currently.precipProbability +
           "% chance of rain."
       );
